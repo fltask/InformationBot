02100e30fa02c3446ed9ed97d090143fe54baa80
Dmitriy S. <fltask.pro@gmail.com>
2025-07-17 02:05:44 +0300

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -10,36 +10,36 @@ from alembic import context
 # Загружаем переменные окружения из файла .env
 load_dotenv()
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# это объект конфигурации Alembic, который предоставляет
+# доступ к значениям в используемом .ini файле.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Интерпретируем файл конфигурации для логирования Python.
+# Эта строка настраивает логгеры.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
+# добавьте объект MetaData вашей модели здесь
+# для поддержки 'autogenerate'
 from database.models import Base
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
+# другие значения из конфигурации, определенные потребностями env.py,
+# могут быть получены:
 # my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# ... и т.д.
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+    """Запускает миграции в 'офлайн' режиме.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    Это настраивает контекст только с URL
+    и без Engine, хотя Engine также допустим
+    здесь. Пропуская создание Engine
+    нам даже не нужен DBAPI.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
+    Вызовы context.execute() здесь выводят данную строку в
+    выходной скрипт.
 
     """
     # Получаем строку подключения из переменных окружения
@@ -59,10 +59,10 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Запускает миграции в 'онлайн' режиме.
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
+    В этом сценарии нам нужно создать Engine
+    и связать соединение с контекстом.
 
     """
     # Получаем строку подключения из переменных окружения
